66e05e551d634108bf732746a4904244894ba1a4
prakash <kuruva.prakash@zapuza.in>
2024-09-11 12:51:02 +0530

    added the change passsword feature for the user,that is only accessible to the requested user

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 
 import home.views as v
 from home.customViews import (authView)
-from home.views import (HomeView)
+from home.views import (HomeView,PasswordResetView)
 
 urlpatterns = [
     path('', HomeView.as_view(), name='dashboard'),
     path('accounts/login/', authView.LoginView.as_view(), name='login'),
-    path('accounts/logout/', authView.LoginView.as_view(), name='logout')
+    path('accounts/logout/', authView.LogoutView.as_view(), name='logout'),
+    path('reset-password/', PasswordResetView.as_view(), name='password_reset')
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,39 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import update_session_auth_hash
 
 
 class HomeView(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, "dashboard.html")
+
+
+class PasswordResetView(LoginRequiredMixin, View):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'login'
+    def get(self, request):
+        return render(request, 'auth/reset_password.html')
+
+    def post(self, request):
+        current_password = request.POST.get('current_password')
+        new_password = request.POST.get('new_password')
+        confirm_password = request.POST.get('confirm_password')
+
+        user = request.user
+
+        if not user.check_password(current_password):
+            messages.error(request, 'Current password is incorrect.')
+        elif new_password != confirm_password:
+            messages.error(request, 'New password and confirm password do not match.')
+        else:
+            user.set_password(new_password)
+            user.save()
+            # Keep the user logged in after changing the password
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Your password has been updated successfully!')
+            return redirect('dashboard')  # Redirect to the dashboard or another page
+
+        # If there's an error, render the page again with error messages
+        return render(request, 'auth/reset_password.html')
