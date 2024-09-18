8b22414d9565591ca3c79f167febf4630ea3c0ba
fairoosa <fairoosa02112000@gmail.com>
2024-09-18 12:04:31 +0530

    Create log folder for tracking errors and info

--- home/views.py
+++ home/views.py
@@ -4,13 +4,23 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
+from django.conf import settings
+import logging.config
+
+# Apply logging configuration
+logging.config.dictConfig(settings.LOGGING)
+info_logger = logging.getLogger('info_logger')
+error_logger = logging.getLogger('error_logger')
+
 class HomeView(LoginRequiredMixin, View):
     def get(self, request):
+        info_logger.info('HomeView GET request by user: %s', request.user.username)
         return render(request, "dashboard.html")
 
 
 class Custom404View(View):
     def get(self, request, exception=None):
+        error_logger.error('404 error occurred for URL: %s', request.path)
         return render(request, '404.html', status=404)
 
 
@@ -18,6 +28,7 @@ class PasswordResetView(LoginRequiredMixin, View):
     login_url = '/accounts/login/'
     redirect_field_name = 'login'
     def get(self, request):
+        info_logger.info('PasswordResetView GET request by user: %s', request.user.username)
         return render(request, 'auth/reset_password.html')
 
     def post(self, request):
@@ -29,14 +40,17 @@ class PasswordResetView(LoginRequiredMixin, View):
 
         if not user.check_password(current_password):
             messages.error(request, 'Current password is incorrect.')
+            error_logger.error('Failed password reset attempt for user %s: Incorrect current password', user.username)
         elif new_password != confirm_password:
             messages.error(request, 'New password and confirm password do not match.')
+            error_logger.error('Failed password reset attempt for user %s: Passwords do not match', user.username)
         else:
             user.set_password(new_password)
             user.save()
             # Keep the user logged in after changing the password
             update_session_auth_hash(request, user)
             messages.success(request, 'Your password has been updated successfully!')
+            info_logger.info('Password updated successfully for user %s', user.username)
             return redirect('dashboard')  # Redirect to the dashboard or another page
 
         # If there's an error, render the page again with error messages
