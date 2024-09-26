f61130d9c4f0295468e74fef63d2df8cb93373de
fairoosa <fairoosa02112000@gmail.com>
2024-09-26 10:19:01 +0530

    Create ARN Tracking Model

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from home.models import EmailErrorLog
+from home.models import EmailErrorLog,ARNTracking
 
 # Register the model in the admin panel
 @admin.register(EmailErrorLog)
@@ -8,3 +8,13 @@ class EmailErrorLogAdmin(admin.ModelAdmin):
     search_fields = ('subject', 'to_email')
     list_filter = ('timestamp',)
     ordering = ('-timestamp',)
+
+
+@admin.register(ARNTracking)
+class ARNTrackingAdmin(admin.ModelAdmin):
+    list_display = ('sl_no', 'trade_name', 'gstn', 'arn_number', 'dated', 'assigned_to', 'reply_due_date', 'current_status')
+    search_fields = ('trade_name', 'gstn', 'arn_number')
+    list_filter = ('dated', 'reply_due_date', 'current_status')
+    ordering = ('-dated',)
+
+
--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class EmailErrorLog(models.Model):
@@ -9,3 +10,27 @@ class EmailErrorLog(models.Model):
 
     class Meta:
         verbose_name_plural = "Email Error Logs"
+
+
+class ARNTracking(models.Model):
+    sl_no = models.PositiveIntegerField(unique=True)
+    trade_name = models.CharField(max_length=255)
+    gstn = models.CharField(
+        max_length=15,
+        validators=[RegexValidator(
+            regex=r'^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$',
+            message="Invalid GSTN format"
+        )]
+    )
+    purpose = models.TextField()
+    arn_number = models.CharField(max_length=50, unique=True)
+    dated = models.DateField()
+    assigned_to = models.CharField(max_length=255)
+    reply_due_date = models.DateField()
+    current_status = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = 'ARN Tracking Records'
+
+    def __str__(self):
+        return f"ARN Tracking: {self.sl_no}"
