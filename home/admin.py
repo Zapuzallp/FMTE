4e46851cab1405b5abbd1b7d581dbbb3d2f37a46
fairoosa <fairoosa02112000@gmail.com>
2024-09-30 20:28:35 +0530

    Resolve Conflicts

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from home.models import EmailErrorLog,ARNTracking
+from home.models import EmailErrorLog, ARNTracking, Employee
 
 # Register the model in the admin panel
 @admin.register(EmailErrorLog)
@@ -10,11 +10,15 @@ class EmailErrorLogAdmin(admin.ModelAdmin):
     ordering = ('-timestamp',)
 
 
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['fathers_name', 'position', 'status', 'date_of_joining']
+
+
 @admin.register(ARNTracking)
 class ARNTrackingAdmin(admin.ModelAdmin):
     list_display = ('sl_no', 'trade_name', 'gstn', 'arn_number', 'dated', 'assigned_to', 'reply_due_date', 'current_status')
     search_fields = ('trade_name', 'gstn', 'arn_number')
     list_filter = ('dated', 'reply_due_date', 'current_status')
     ordering = ('-dated',)
-
 
--- home/models.py
+++ home/models.py
@@ -12,6 +12,49 @@ class EmailErrorLog(models.Model):
         verbose_name_plural = "Email Error Logs"
 
 
+class Employee(models.Model):
+    # Field for timestamp of record creation
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    # Father's name
+    fathers_name = models.CharField(max_length=100, blank=True)
+
+    # PAN Document Upload
+    upload_pan = models.FileField(upload_to='documents/pan/', blank=True, null=True)
+
+    # Aadhar Document Upload
+    upload_aadhar = models.FileField(upload_to='documents/aadhar/', blank=True, null=True)
+
+    # Cancelled Cheque, Passbook, or Statement Upload
+    cancelled_cheque_passbook_statement = models.FileField(upload_to='documents/bank_documents/', blank=True, null=True)
+
+    # Bank details
+    bank_name = models.CharField(max_length=100)
+    bank_branch = models.CharField(max_length=100)
+    bank_account_number = models.CharField(max_length=20, validators=[
+        RegexValidator(regex=r'^\d{9,18}$', message='Enter a valid bank account number')
+    ])
+    bank_ifsc_code = models.CharField(max_length=11, validators=[
+        RegexValidator(regex=r'^[A-Z]{4}0[A-Z0-9]{6}$', message='Enter a valid IFSC code')
+    ])
+
+    # Employment details
+    date_of_joining = models.DateField()
+    position = models.CharField(max_length=100)
+    end_date = models.DateField(blank=True, null=True)
+
+    # Employment status
+    STATUS_CHOICES = [
+        ('Active', 'Active'),
+        ('Inactive', 'Inactive'),
+        ('Terminated', 'Terminated'),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f"{self.fathers_name}'s Employment Record"
+
+
 class ARNTracking(models.Model):
     sl_no = models.PositiveIntegerField(unique=True)
     trade_name = models.CharField(max_length=255)
