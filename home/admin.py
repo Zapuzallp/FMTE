baece7e12c7bb97eda9fe14c0ab2e1051782d76c
fairoosa <fairoosa02112000@gmail.com>
2024-09-30 21:05:56 +0530

    Resolve Conflicts

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from home.models import EmailErrorLog
-from home.models import Employee
+from home.models import Employee, ARNTracking
 
 # Register the model in the admin panel
 @admin.register(EmailErrorLog)
@@ -14,3 +14,12 @@ class EmailErrorLogAdmin(admin.ModelAdmin):
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ['fathers_name', 'position', 'status', 'date_of_joining']
+
+
+@admin.register(ARNTracking)
+class ARNTrackingAdmin(admin.ModelAdmin):
+    list_display = ('sl_no', 'trade_name', 'gstn', 'arn_number', 'dated', 'assigned_to', 'reply_due_date', 'current_status')
+    search_fields = ('trade_name', 'gstn', 'arn_number')
+    list_filter = ('dated', 'reply_due_date', 'current_status')
+    ordering = ('-dated',)
+
